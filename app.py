ba5a2f27074b5e91f1f740fadfb63447432a9542
BerendStudent <144696095+BerendStudent@users.noreply.github.com>
2025-01-04 10:59:18 +0100

    Moves files to make flask file relations functional

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,render_template,request,jsonify
+from flask import Flask,redirect,render_template,request,jsonify,send_from_directory,url_for
 
 
 app = Flask(__name__)
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def load_main_page():
     return render_template('index.html')
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
