01cf92bec2df06f1fa91d020776fdb731e647eb2
BerendStudent <144696095+BerendStudent@users.noreply.github.com>
2025-01-05 18:00:55 +0100

    Adds about page, adds navbar, makes possibleImages automatically load from images folder. Alters functionality to use await

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,redirect,render_template,request,jsonify,send_from_directory,url_for
-
+import os
 
 app = Flask(__name__)
 
@@ -7,6 +7,25 @@ app = Flask(__name__)
 def load_main_page():
     return render_template('index.html')
 
+
+@app.route("/guessr")
+def render_work_page():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def render_about_page():
+    return render_template('about.html')
+
+
+@app.route("/possibleguesses", methods=["GET"])
+def supply_image_names():
+    choices = []
+    with os.scandir('./static/images') as entries:
+        for entry in entries:
+            filename, extension = os.path.splitext(entry.name)
+            choices.append(filename)
+    return jsonify(choices), 200
 
 
 if __name__ == "__main__":
